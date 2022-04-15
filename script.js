5c48870a043d5697ae759b9829a039a110affdcd
Pina Gomet <101363048+pgomet@users.noreply.github.com>
2022-04-14 22:47:30 -0400

    created a 'generate password' function with required characters

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Assignment code here
-
+// create a function to generate a password
+function generatePassword() {
+  var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V","W", "X", "Y", "Z"];
+  var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"];
+  var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?"];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
