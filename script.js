6b4411b45312226fd1b0659529afeccab726ec47
Pina Gomet <101363048+pgomet@users.noreply.github.com>
2022-04-14 23:03:08 -0400

    added in number of chaarcters & lowercase letters

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 // Assignment code here
-// create a function to generate a password
+// Create a function to generate a password
 function generatePassword() {
   var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V","W", "X", "Y", "Z"];
   var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"];
   var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?"];
+
+// Enter the character qualities
+numberOfCharacters = prompt("Please choose between 8 - 128 characters.");
+  if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+    return ("Please pick a valid number of characters.");
+  } else if (isNaN(numberOfCharacters)) {
+    numberOfCharacters = prompt("Please enter a valid number.");
+  }
+    else {
+      alert("Your password will be " + numberOfCharacters + " characters long.");
+    }
+
+containsLowercase = confirm("Do you want your password to contain lowercase characters?");
+  if (containsLowercase) {
+    var turnToLowercase = alert("Your password will contain lowercase characters.");
+  }
+    else {
+      alert("Your password will NOT contain lowercase characters.");
+    }
+
 }
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
